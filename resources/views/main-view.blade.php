71b1909d6de44991d6b82179dc8a81c939bfdd40
Marc De Gagné <22817419+MDGDesign@users.noreply.github.com>
2023-04-08 17:05:14 -0400

    Added code

--- resources/views/main-view.blade.php
+++ resources/views/main-view.blade.php
@@ -1,3 +1,16 @@
+<?php
 
-
-<!-- Livewire call for the modal -->
+<div class="form-row auto-grid-placement">
+    <x-form.field class="col-span-full">
+        <div class="flex">
+            <x-form.label for=""><h4>Habitat</h4></x-form.label>
+            <!-- Livewire call for the modal -->
+            <x-form.btn-add-new-value
+                modal="models.habitat-create"
+                title="Add a new habitat"
+                class="add-modal">+</x-form.btn-add-new-value>
+        </div>
+        <!-- Displays the habitat types with edit and delete -->
+        @livewire( "models.habitat-types" )
+    </x-form.field>
+</div>
